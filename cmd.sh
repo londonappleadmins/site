568f3dc29c21ed41d62c1c22ab05d0e5f60de1db
macmule <2464974+macmule@users.noreply.github.com>
2023-01-03 22:55:07 +0000

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,8 @@
-export JEKYLL_VERSION=3.8.5
+export JEKYLL_VERSION=4.2.2
 docker run --rm \
   --volume="$PWD:/srv/jekyll" \
   --volume="$PWD/vendor/bundle:/usr/local/bundle" \
   -e JEKYLL_ENV=production \
-  -p 8000:4000 \
+  -p 4000:4000 \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   ${@}
